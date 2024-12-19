4055ac7f244b80ec930aeee50f4494762d232f0b
Juan Felipe Rendon Herrera <felipe28769@gmail.com>
2024-12-19 00:00:42 -0500

    Doc tareas.py

--- Gestor de Tareas/gestor/tareas.py
+++ Gestor de Tareas/gestor/tareas.py
@@ -1,10 +1,15 @@
+# Clase que gestiona las tareas
 class Tareas:
     def __init__(self) -> None:
+        # Diccionario para almacenar las tareas, con el id como clave
         self.tareas = {}
+        # Variable para generar un id único para cada tarea
         self.id_tarea = 0
 
+    # Método para agregar una nueva tarea
     def agregar_tarea(self, titulo, descripcion, fecha_limite, etiqueta) -> None:
         self.id_tarea += 1
+        # Almacena la tarea en el diccionario usando el id generado
         self.tareas[self.id_tarea] = {
             "Id": self.id_tarea,
             "Titulo": titulo,
@@ -13,6 +18,7 @@ class Tareas:
             "Etiqueta": etiqueta,
         }
 
+    # Método para listar todas las tareas registradas
     def listar_tareas(self) -> str | None:
         if not self.tareas:
             print("No hay tareas registradas")
@@ -20,12 +26,14 @@ class Tareas:
         for tarea in self.tareas.values():
             self.mostrar_tareas(tarea)
 
+    # Método para editar una tarea existente
     def editar_tareas(
         self, id, nuevo_titulo, nueva_descripcion, nueva_fecha_limite, nueva_etiqueta
     ) -> bool:
         if not self.tareas:
             print("No hay tareas registradas")
             return False
+        # Actualiza la tarea con el id especificado
         self.tareas[id].update(
             {
                 "Titulo": nuevo_titulo,
@@ -36,6 +44,7 @@ class Tareas:
         )
         return True
 
+    # Método para eliminar una tarea existente
     def eliminar_tarea(self, id: int) -> bool:
         if not self.tareas:
             print("No hay tareas registradas")
@@ -46,6 +55,7 @@ class Tareas:
                 return True
         return False
 
+    # Método para buscar tareas por etiqueta
     def buscar_tarea_por_etiqueta(self, etiqueta) -> list:
         return [
             etiquetas
@@ -53,9 +63,11 @@ class Tareas:
             if etiquetas["Etiqueta"] == etiqueta
         ]
 
+    # Método para buscar una tarea por su id
     def buscar_tarea_por_id(self, id) -> dict | None:
         return self.tareas.get(id, None)
 
+    # Método para mostrar los detalles de una tarea
     def mostrar_tareas(self, tarea) -> None:
         print(
             f"Id: {tarea['Id']}",
