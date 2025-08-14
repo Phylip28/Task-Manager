8de36a233a75ed7902028691301b8ba32191af3c
Phylip28 <felipe28769@gmail.com>
2025-08-14 13:41:51 -0500

    Feat: Switch to production-style Docker Compose

--- deploy.sh
+++ deploy.sh
@@ -5,25 +5,16 @@ set -e
 
 # Environment variables
 IMAGE_NAME="ghcr.io/phylip28/task-manager:main"
-CONTAINER_NAME="fastapi_container"
 
-echo "Init deployment process of api: $CONTAINER_NAME"
+echo "Init deployment with Docker Compose"
 
 echo 1. Download the latest image from GitHub Container Registry
 docker pull $IMAGE_NAME
 
-echo 2. Stop and remove the existing container if it exists
-if [ $(docker ps -q -f name=$CONTAINER_NAME) ]; then
-  docker stop $CONTAINER_NAME
-  docker rm $CONTAINER_NAME
-fi
+echo 2. Update services with new image
+docker-compose up -d
 
-echo 3. Build and run the new container
-docker run -d --name $CONTAINER_NAME \
-    -p 127.0.0.1:8000:8000 \
-    $IMAGE_NAME
+echo 3. Remove dangling images
+docker image prune -f
 
-echo 4. Remove dangling images
-docke image prune -f
-
-echo 5. Deployment completed successfully!
+echo 4. Deployment completed successfully!
